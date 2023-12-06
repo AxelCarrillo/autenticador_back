b1a4da980ec37c350aa8a9a19364074122d2be2a
AxelCarrillo <122809603+AxelCarrillo@users.noreply.github.com>
2023-12-06 09:45:15 -0600

    CREATED

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# autenticador_back
+# AUTHENTIFICATOR BACKEND
+Api rest para poder autentificar
+
+SQLite3, FastAPI, HTTPAuthentificator
+
+gunicorn -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 main:app
---
+++ sql/usuarios.sql
@@ -0,0 +1,10 @@
+-- Crear la tabla de usuarios con información de autenticación
+CREATE TABLE IF NOT EXISTS usuarios (
+    username VARCHAR (100) PRIMARY KEY NOT NULL,
+    password VARCHAR (32) NOT NULL,
+    token VARCHAR(65) NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- password 123456789
+INSERT INTO usuarios (username, password, token) VALUES ('usuario@email.com', '25f9e794323b453885f5181f1b624d0b', '1234');
